116fdc999f77e88c73c458f800fade5d42faafbe
unknown <satya-1298>
2023-06-17 08:13:20 +0530

    [Satya]Added:Constructor programs

---
+++ Constructors/ConstructorExample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    public class ConstructorExample
+    {
+        //It is having same name as the class name
+        //It is similar to method however it does not have return type
+        //no.of constructors can be created but with different parameters
+        public ConstructorExample() 
+        {
+            Console.WriteLine("hello");
+        }
+        public ConstructorExample(string name) 
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ Constructors/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConstructorExample example = new ConstructorExample();  
+            ConstructorExample example2 = new ConstructorExample("satya"); 
+        }
+    }
+}
